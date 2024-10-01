8f4b51f52d4b0dacc6a28c41b2b17e40b3a6d08d
kouachi-c <ekoncorneille@gmail.com>
2024-10-01 13:10:48 +0200

    adding freeRTOS

--- code/main.cpp
+++ code/main.cpp
@@ -1,7 +1,6 @@
 
 
 #include <neorv32.h>
-#include "Neorv32_BNO055.h"
 #include "Neorv32_ST7789.h"
 
 #define cs 0
@@ -13,25 +12,15 @@
 int main() {
     // Sensor and screen initialization
     Neorv32_ST7789 screen( cs, dc, rst);
-    Neorv32_BNO055 bno2(BNO055_ID);
 
     // Parameters for the sensor
-    neorv32_vector_type_t vector = VECTOR_ACCELEROMETER;
-    neorv32_bno055_unit_sel_t unitSel = UNIT_SEL_AND_CEl_DPS_DEG_MS2;
     uint8_t numDataBytes = 3;
     int16_t *data = new int16_t[numDataBytes];
-
-    bno2.begin(OPERATION_MODE_AMG, unitSel);
 
     screen.init(240, 240);
     screen.begin();
     screen.enableDisplay(true);
 
-    while(1) {
-        bno2.getVector(vector, data);
-        screen.sendCommand(ST77XX_MADCTL, data, numDataBytes);
-
-    }
 
 
 
